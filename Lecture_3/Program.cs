5055f70c4d23dd565eed8e1d2a2fc5d73c776d3c
marissa36 <marissa36@yandex.ru>
2024-01-14 21:20:21 +0300

    add Lesson3:task1

---
+++ Lecture_3/Program.cs
@@ -0,0 +1,11 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numerator = a * b;
+    int denominator = c + d;
+    double result = numerator / denominator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.Write(result);
+
